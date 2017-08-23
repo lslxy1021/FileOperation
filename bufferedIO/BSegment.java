5400f185044b44ff169fb69bf86a88eb81102c3b
lslxy1021 <693408355@qq.com>
2017-08-24 07:58:27 +0800

    Update BSegment.java

--- bufferedIO/BSegment.java
+++ bufferedIO/BSegment.java
@@ -8,48 +8,50 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 
 public class BSegment {
-	
-    private static String destPath;//分割文件目标文件路径
-    private static File destFile;//分割文件目标文件夹
-    private static File outFile;//分割文件
-    private static File outInfoFile;//分割信息文件，合并时需要
-    private static BufferedInputStream inSource;//原文件输入流
-    private static BufferedOutputStream outDest;//分割文件输入流，字节流
-    private static FileWriter outInfo;//分割信息文件输入流,字符流
-    
+
+	private static String destPath;// 分割文件目标文件路径
+	private static File destFile;// 分割文件目标文件夹
+	private static File outFile;// 分割文件
+	private static File outInfoFile;// 分割信息文件，合并时需要
+	private static BufferedInputStream inSource;// 原文件输入流
+	private static BufferedOutputStream outDest;// 分割文件输入流，字节流
+	private static FileWriter outInfo;// 分割信息文件输入流,字符流
+
 	public static void fileSeperate(File file, int number, String suffix) throws Exception {
-		destPath = file.getParentFile().toString() + File.separator + file.getName().substring(0,file.getName().indexOf("."));
+		destPath = file.getParentFile().toString() + File.separator
+				+ file.getName().substring(0, file.getName().indexOf("."));
 		destFile = new File(destPath);
-		if (!destFile.exists()) {  
-            destFile.mkdirs();
-        }  
-		outInfoFile = new File(destFile, file.getName().substring(0,file.getName().indexOf(".")) + suffix +".txt");
+		if (!destFile.exists()) {
+			destFile.mkdirs();
+		}
+		outInfoFile = new File(destFile, file.getName().substring(0, file.getName().indexOf(".")) + suffix + ".txt");
 		outInfo = new FileWriter(outInfoFile);
-		inSource = new BufferedInputStream(new FileInputStream(file),64*1024);
+		inSource = new BufferedInputStream(new FileInputStream(file), 64 * 1024);
 		long fileLength = file.length();
 		long size = fileLength / number + 1;
 		long eIndex = 0;
 		long bIndex = 0;
 		for (int i = 0; i < number; i++) {
-			outFile = new File(destFile, file.getName().substring(0,file.getName().indexOf(".")) + "part" + i + suffix);
-			outDest = new BufferedOutputStream(new FileOutputStream(outFile),64*1024);//设置缓冲区大小至64KB(默认是8KB)
-			eIndex +=  size;
+			outFile = new File(destFile,
+					file.getName().substring(0, file.getName().indexOf(".")) + "part" + i + suffix);
+			outDest = new BufferedOutputStream(new FileOutputStream(outFile), 64 * 1024);// 设置缓冲区大小至64KB(默认是8KB)
+			eIndex += size;
 			eIndex = (eIndex > fileLength) ? fileLength : eIndex;
-			while(bIndex < eIndex) {
+			while (bIndex < eIndex) {
 				outDest.write(inSource.read());
 				bIndex++;
 			}
-			outInfo.write(outFile.getAbsolutePath());	
+			outInfo.write(outFile.getAbsolutePath());
 			outInfo.write(System.getProperty("line.separator"));
 			outDest.flush();
 			outInfo.flush();
 		}
 		outDest.close();
-		inSource.close();	
+		inSource.close();
 		outInfo.close();
 	}
 
 	public static void main(String[] args) {
-		
+
 	}
 }
